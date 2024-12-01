c82339dc0e6afea6491c014b176ad56ab1c92c04
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-01 12:24:27 +0100

    .

--- day1/Program.cs
+++ day1/Program.cs
@@ -13,47 +13,54 @@ string test_data = """
 string test_result = "11";
 var dr = new Tools.DataReader();
 
-var result = await dr.read_data("https://adventofcode.com/2024/day/1/input");
+var result = test_data;//await dr.read_data("https://adventofcode.com/2024/day/1/input");
 
-var list1 = new List<int>();
-var list2 = new List<int>();
+void day1(string result)
+{  //day1 part1
+    var list1 = new List<int>();
+    var list2 = new List<int>();
 
-var lines = result.Split("\n");
-foreach (var line in lines)
-{
-    if(string.IsNullOrEmpty(line))
-        continue;
+    var lines = result.Split("\n");
+    foreach (var line in lines)
+    {
+        if(string.IsNullOrEmpty(line))
+            continue;
     
-    Console.WriteLine(line);
+        Console.WriteLine(line);
     
-    var numbers = line.Split("   ");
-    list1.Add(int.Parse(numbers[0]));
-    list2.Add(int.Parse(numbers[1]));
+        var numbers = line.Split("   ");
+        list1.Add(int.Parse(numbers[0]));
+        list2.Add(int.Parse(numbers[1]));
+    }
+
+    var ordered1 = list1.OrderBy(x => x).ToList();
+    var ordered2 = list2.OrderBy(x => x).ToList();
+    var sum = 0;
+    for (int i = 0; i < ordered1.Count; i++)
+    {
+        var num1 = ordered1[i];
+        var num2 = ordered2[i];
+    
+        if (num1 == num2)
+            continue;
+    
+        if(num1 > num2)
+        {
+            sum += num1 - num2;
+        }
+        else
+        {
+            sum += num2 - num1;
+        }
+    }
+
+    Console.WriteLine(sum);
 }
 
-var ordered1 = list1.OrderBy(x => x).ToList();
-var ordered2 = list2.OrderBy(x => x).ToList();
-var sum = 0;
-for (int i = 0; i < ordered1.Count; i++)
-{
-    var num1 = ordered1[i];
-    var num2 = ordered2[i];
+{ //day1 part2
     
-    if (num1 == num2)
-        continue;
     
-    if(num1 > num2)
-    {
-        sum += num1 - num2;
-    }
-    else
-    {
-        sum += num2 - num1;
-    }
 }
-
-Console.WriteLine(sum);
-
 
 
 
