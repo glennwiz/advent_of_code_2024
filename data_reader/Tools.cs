d630f236768742ced0be0678c56bb0e8ea04eef1
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-01 11:52:59 +0100

    the beginning

---
+++ data_reader/Tools.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+
+namespace Tools;
+
+public class DataReader
+{
+    private string folderPath = @"C:\dev\advent_of_code_2024\data\";
+    public async Task<string> read_data(string path = "")
+    {
+        var data = check_if_data_exists(path);
+        if(data != "")
+        {
+            return data;
+        }
+        
+        using var client = new HttpClient();
+        var cookie = File.ReadAllText("cookie.369");
+        
+        client.DefaultRequestHeaders.Add("Cookie",cookie);
+        var result = await client.GetStringAsync(path);
+
+        write_data(path, result);
+        
+        return result;
+    }
+    
+    private void write_data(string path, string data)
+    {
+        var result = string.Concat(path.Where(char.IsDigit)); 
+        Console.WriteLine(result);
+    
+        Directory.CreateDirectory(folderPath);
+
+        File.WriteAllText(Path.Combine(folderPath, result), data);
+    }
+    
+    private string check_if_data_exists(string path)
+    {
+        var result = string.Concat(path.Where(char.IsDigit)); 
+        var filePath = Path.Combine(folderPath, result);
+        
+        if (File.Exists(filePath))
+        {
+            return File.ReadAllText(filePath);
+        }
+        else
+        {
+            return "";
+        }
+    }
+}
---
+++ day1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tools;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var dr = new Tools.DataReader();
+
+var result  = await dr.read_data("https://adventofcode.com/2024/day/1/input");
+
+Console.WriteLine(result);
+
+
+
+
+string test_data = """
+                   3   4
+                   4   3
+                   2   5
+                   1   3
+                   3   9
+                   3   3
+                   """;
+
+string test_result = "11";
