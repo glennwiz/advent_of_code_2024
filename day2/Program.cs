0bbc71d01bac2bffa12fd47a09b8704a910c0150
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-03 00:10:19 +0100

    day2 part2

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,85 +1,165 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-var dr = new Tools.DataReader();
+using System.Net.WebSockets;
 
-//var resultText = File.ReadAllText("C:\\dev\\advent_of_code_2024\\data\\day2_test");
-var resultText = await dr.read_data("https://adventofcode.com/2024/day/2/input");
-
-Console.WriteLine(resultText);
-
-var lines = resultText.Split("\n");
-
-var safeConter = 0;
-
-foreach (var line in lines)
+internal class Program
 {
-    if(string.IsNullOrEmpty(line))
-        continue;
-
-    var numbers = line.Split(" ");
-    
-    var nums = numbers.Select(x => int.Parse(x)).ToList();
-    var NotSafe = false; 
-    int? previousNum = null;
-    
-    bool breakerUp = false;
-    bool breakerDown = false;
-    
-    for (int i = 0; i < numbers.Length -1; i++)
+    private static async Task Main(string[] args)
     {
-        if (nums[i] == nums[i + 1])
-        {
-            NotSafe = true;
-            break;
-        }
-        
-        if (nums[i] < nums[i + 1])
+        Console.WriteLine("Hello, World!");
+        var dr = new Tools.DataReader();
+
+        //var resultText = File.ReadAllText("C:\\dev\\advent_of_code_2024\\data\\day2_test");
+        var resultText = await dr.read_data("https://adventofcode.com/2024/day/2/input");
+
+        Console.WriteLine(resultText);
+
+        var lines = resultText.Split("\n");
+
+        var safeCounter = 0;
+        part1();
+        part2();
+
+        void part2() 
         { 
-            breakerUp = true;
-            var n = nums[i] - nums[i + 1];
-            if (n > 3)
+            foreach (var line in lines)
             {
-                NotSafe = true;
-                break;
-            }
-            
-            if (n < -3)
-            {
-                NotSafe = true;
-            }
-        }
-        
-        if (nums[i] > nums[i + 1])
-        {
-            breakerDown = true;
-            
-            var n = nums[i] - nums[i + 1];
-            if (n > 3)
-            {
-                NotSafe = true;
-                break;
+                if (string.IsNullOrEmpty(line)) continue;
+                if (Damp(line))
+                    safeCounter++;
             }
 
-            if (n < -3)
+            Console.WriteLine(safeCounter);       
+
+            static bool Damp(string line)
             {
-                NotSafe = true;
+                var nums = line.Split(" ").Select(x => int.Parse(x)).ToList();
+
+                if (GreenLight(nums))
+                    return true;
+
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    if (GreenLight(nums, i))
+                        return true;
+                }
+
+                return false;
+            }
+
+            static bool GreenLight(List<int> nums, int? errorSlot = null)
+            {
+                var checkNodes = errorSlot.HasValue ? GetNewList(nums, errorSlot.Value) : nums;
+
+                bool? isIncreasing = null;
+
+                for (int i = 0; i < checkNodes.Count - 1; i++)
+                {
+                    int diff = checkNodes[i + 1] - checkNodes[i];
+                
+                    if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
+                        return false;
+                              
+                    if (isIncreasing == null)
+                        isIncreasing = diff > 0;
+
+                    else if ((diff > 0) != isIncreasing)
+                        return false;
+                }
+
+                return true;
+            }
+            
+            static List<int> GetNewList(List<int> numbers, int value)
+            {
+                var returnList = new List<int>();
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (i == value)
+                        continue;
+
+                    returnList.Add(numbers[i]);
+                }
+
+                return returnList;
             }
         }
-        
-        if(breakerUp && breakerDown)
+
+
+        void part1()
         {
-            NotSafe = true;
-            break;
+            foreach (var line in lines)
+            {
+
+                if (string.IsNullOrEmpty(line))
+                    continue;
+
+                var numbers = line.Split(" ");
+
+                var nums = numbers.Select(x => int.Parse(x)).ToList();
+                var NotSafe = false;
+
+                var breakerUp = false;
+                var breakerDown = false;
+
+                for (var i = 0; i < numbers.Length - 1; i++)
+                {
+                    if (nums[i] == nums[i + 1])
+                    {
+                        NotSafe = true;
+                        break;
+                    }
+
+                    if (nums[i] < nums[i + 1])
+                    {
+                        breakerUp = true;
+                        var n = nums[i] - nums[i + 1];
+                        if (n > 3)
+                        {
+                            NotSafe = true;
+                            break;
+                        }
+
+                        if (n < -3)
+                        {
+                            NotSafe = true;
+                        }
+                    }
+
+                    if (nums[i] > nums[i + 1])
+                    {
+                        breakerDown = true;
+
+                        var n = nums[i] - nums[i + 1];
+                        if (n > 3)
+                        {
+                            NotSafe = true;
+                            break;
+                        }
+
+                        if (n < -3)
+                        {
+                            NotSafe = true;
+                        }
+                    }
+
+                    if (breakerUp && breakerDown)
+                    {
+                        NotSafe = true;
+                        break;
+                    }
+                }
+
+                if (NotSafe)
+                {
+                    Console.WriteLine(line);
+                    continue;
+                }
+
+                safeCounter++;
+            }
+
+            Console.WriteLine(safeCounter);
+            safeCounter = 0;
         }
-    }
-    
-    if (NotSafe)
-    {
-        Console.WriteLine(line);
-        continue;
-    }
-    
-    safeConter++;
+    }    
 }
-
-Console.WriteLine(safeConter);
