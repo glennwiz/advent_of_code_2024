1b79599fea2ae0aff727d3bf7eae1309e10dce4f
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-02 16:38:15 +0100

    data

---
+++ day2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var dr = new Tools.DataReader();
+
+var result = await dr.read_data("https://adventofcode.com/2024/day/2/input");
