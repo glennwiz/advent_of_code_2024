14d78023135a6bca7081caa4836d4bba39497dd1
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-04 19:19:29 +0100

    Tenker vi gjør convay her, alle rettninger

--- day4/Program.cs
+++ day4/Program.cs
@@ -1,7 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string test = """
+char[,] CreateGrid(string data)
+{
+    string[] lines = data.Split('\n', StringSplitOptions.RemoveEmptyEntries); // Remove empty lines
+    int rows = lines.Length;
+    int cols = lines[0].Trim().Length;
+
+    char[,] grid = new char[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {        
+        for (int j = 0; j < cols - 1; j++)
+            grid[i, j] = lines[i][j];
+    }
+    return grid;
+}
+
+string input = """
 MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -13,3 +28,16 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX
 """;
+
+char[,] TheConvayGrid = CreateGrid(input);
+int rows = TheConvayGrid.GetLength(0);
+int cols = TheConvayGrid.GetLength(1);
+Console.WriteLine("Convay rows" + rows);
+Console.WriteLine("Convay cols" + cols);
+foreach (var item in TheConvayGrid)
+{
+    Console.Write(item);
+}
+
+//this is convay game of life but we keep going and looking in the snowflake pattern from the center 0(X) + 1(M) + 2(A) + 3(S) in all directions
+
