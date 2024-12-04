ea77c84ae50e5dfc089c2d576e825b97b02b55a2
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-04 19:48:27 +0100

    Day4 p1

--- day4/Program.cs
+++ day4/Program.cs
@@ -1,43 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+
+var dr = new Tools.DataReader();
+var resultText = await dr.read_data("https://adventofcode.com/2024/day/4/input");
+
 Console.WriteLine("Hello, World!");
+
+var input = """
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
+        """;
+
+var TheConvayGrid = CreateGrid(resultText);
+var count = CountXMASWithDebug(TheConvayGrid);
+Console.WriteLine($"\nFinal Count: 'XMAS' found {count} times.");
 
 char[,] CreateGrid(string data)
 {
-    string[] lines = data.Split('\n', StringSplitOptions.RemoveEmptyEntries); // Remove empty lines
-    int rows = lines.Length;
-    int cols = lines[0].Trim().Length;
+    var lines = data.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+    var rows = lines.Length;
+    var cols = lines[0].Trim().Length;
 
-    char[,] grid = new char[rows, cols];
-    for (int i = 0; i < rows; i++)
-    {        
-        for (int j = 0; j < cols - 1; j++)
-            grid[i, j] = lines[i][j];
+    var grid = new char[rows, cols];
+    for (var i = 0; i < rows; i++)
+    {
+        var line = lines[i].Trim();
+        for (var j = 0; j < cols; j++)
+            grid[i, j] = line[j];
     }
     return grid;
 }
 
-string input = """
-MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX
-""";
-
-char[,] TheConvayGrid = CreateGrid(input);
-int rows = TheConvayGrid.GetLength(0);
-int cols = TheConvayGrid.GetLength(1);
-Console.WriteLine("Convay rows" + rows);
-Console.WriteLine("Convay cols" + cols);
-foreach (var item in TheConvayGrid)
+int CountXMASWithDebug(char[,] grid)
 {
-    Console.Write(item);
+    var rows = grid.GetLength(0);
+    var cols = grid.GetLength(1);
+    var target = "XMAS";
+    var count = 0;
+
+    int[] dirX = { 0, 1, 1, 1, 0, -1, -1, -1 };
+    int[] dirY = { 1, 1, 0, -1, -1, -1, 0, 1 };
+
+    for (var i = 0; i < rows; i++)
+    {
+        for (var j = 0; j < cols; j++)
+        {             
+            // xxx
+            // xNx
+            // xxx
+            for (var delta = 0; delta < 8; delta++)
+            {
+                if (CheckDirection(grid, i, j, dirX[delta], dirY[delta], target))
+                {
+                    count++;
+                    Console.WriteLine($"Match #{count}: 'XMAS' idx ({i},{j}) vector ({dirX[delta]},{dirY[delta]})");
+                }
+            }
+        }
+    }
+    return count;
 }
 
-//this is convay game of life but we keep going and looking in the snowflake pattern from the center 0(X) + 1(M) + 2(A) + 3(S) in all directions
+bool CheckDirection(char[,] grid, int x, int y, int dirX, int dirY, string target)
+{
+    var rows = grid.GetLength(0);
+    var cols = grid.GetLength(1);
+
+    for (var letterIndex = 0; letterIndex < target.Length; letterIndex++)
+    {
+        var locX = x + letterIndex * dirX;
+        var locY = y + letterIndex * dirY;
+
+        // bounds and letter checks
+        if (locX < 0 || locX >= rows || locY < 0 || locY >= cols || grid[locX, locY] != target[letterIndex])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
