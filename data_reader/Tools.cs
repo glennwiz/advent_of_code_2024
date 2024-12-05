2581d133640418e2e526451981e2446665da0659
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-05 19:04:24 +0100

    day 5 p1

--- data_reader/Tools.cs
+++ data_reader/Tools.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text;
 
 namespace Tools;
 
@@ -8,37 +9,39 @@ public class DataReader
     public async Task<string> read_data(string path = "")
     {
         var data = check_if_data_exists(path);
-        if(data != "")
+        if (data != "")
         {
             return data;
         }
-        
+
         using var client = new HttpClient();
         var cookie = File.ReadAllText("cookie.369");
-        
-        client.DefaultRequestHeaders.Add("Cookie",cookie);
+
+        client.DefaultRequestHeaders.Add("Cookie", cookie);
         var result = await client.GetStringAsync(path);
 
         write_data(path, result);
-        
+
         return result;
     }
-    
+
     private void write_data(string path, string data)
     {
-        var result = string.Concat(path.Where(char.IsDigit)); 
+        var result = string.Concat(path.Where(char.IsDigit));
         Console.WriteLine(result);
-    
+
         Directory.CreateDirectory(folderPath);
 
-        File.WriteAllText(Path.Combine(folderPath, result), data);
+        // Convert to CRLF line endings
+        data = data.Replace("\n", "\r\n").Replace("\r\r\n", "\r\n").TrimEnd();
+        File.WriteAllText(Path.Combine(folderPath, result), data, new UTF8Encoding(false));
     }
-    
+
     private string check_if_data_exists(string path)
     {
-        var result = string.Concat(path.Where(char.IsDigit)); 
+        var result = string.Concat(path.Where(char.IsDigit));
         var filePath = Path.Combine(folderPath, result);
-        
+
         if (File.Exists(filePath))
         {
             return File.ReadAllText(filePath);
---
+++ day5/Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var xx = """
+    47|53
+    97|13
+    97|61
+    97|47
+    75|29
+    61|13
+    75|53
+    29|13
+    97|29
+    53|29
+    61|53
+    97|53
+    61|29
+    47|13
+    75|47
+    97|75
+    47|61
+    75|61
+    47|29
+    75|13
+    53|13
+
+    75,47,61,53,29
+    97,61,53,29,13
+    75,29,13
+    75,97,47,61,53
+    61,13,29
+    97,13,75,29,47
+    """;
+
+var dr = new Tools.DataReader();
+
+var x = await dr.read_data("https://adventofcode.com/2024/day/5/input");
+
+//this is the input string, split in to two parts,,, we split after the fist empty line
+
+var parts = x.Split("\r\n\r\n");
+
+
+//parse the first part
+var rules = parts[0].Split("\n").Select(r => r.Split("|").Select(int.Parse).ToArray()).ToArray();
+
+var dict = new Dictionary<int, List<int>>();
+foreach (var rule in rules)
+{
+    if (!dict.ContainsKey(rule[0]))
+    {
+        dict[rule[0]] = new List<int>();
+    }
+    dict[rule[0]].Add(rule[1]);
+}
+
+var lines = parts[1].Split("\r\n");
+
+var correctLines = new List<List<int>>();
+var listOfNumbers = new List<List<int>>();
+foreach (var item in lines)
+{
+   var nums = item.Split(",").Select(int.Parse).ToList();
+   listOfNumbers.Add(nums);
+}
+
+for (int i = 0; i < listOfNumbers.Count(); i++)
+{
+    var nummerList = listOfNumbers[i];
+    Console.WriteLine("----");
+    var doWeIntersect = false;
+    for (int n = 0; n < nummerList.Count(); n++)
+    {
+        if (n == 0 || doWeIntersect) continue;
+        
+        if (dict.ContainsKey(nummerList[n]))
+        {
+            var nummer = nummerList[n];
+            var list = dict[nummer];
+            //we need to check that all the numbers up to this point is not in the list of numbers
+            var previousNumbers = nummerList.Take(n).ToList();
+            
+            //previuousNumbers is not allowd to be in list
+            var z = list.Intersect(previousNumbers).Any();
+            doWeIntersect = z;
+            Console.WriteLine("result " + z);
+        }
+    }
+
+    if (!doWeIntersect)
+    {
+        correctLines.Add(nummerList);
+    }
+
+}
+
+//let find the center number in the lists
+var centerNumbers = new List<int>();
+foreach (var item in correctLines)
+{
+    var length = item.Count();
+    var center = item[length / 2];
+    centerNumbers.Add(center);
+}
+
+var result = centerNumbers.Aggregate((a, b) => a + b);
+
+Console.WriteLine(result);
