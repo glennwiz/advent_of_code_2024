6a66d44cacba8dc6f3407ddb79f5b9efa4ed13d4
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-07 00:29:28 +0100

    reactors found

--- day6/Program.cs
+++ day6/Program.cs
@@ -22,6 +22,33 @@ var testset = """
 
 var locationOfGuard = findGuard(testset);
 
+var locationOfallReactorstuff = findReactorstuff(resultText);
+
+Console.WriteLine("guard loc" + locationOfGuard);
+foreach (var item in locationOfallReactorstuff)
+{
+    Console.WriteLine(item);
+}
+
+List<(int, int)> findReactorstuff(string resultText)
+{
+    var returndata = new List<(int, int)>();
+    var lines = testset.Split("\n");
+    for (int i = 0; i < lines.Length; i++)
+    {
+        var line = lines[i];
+        for (int j = 0; j < line.Length; j++)
+        {
+            if (line[j] == '#')
+            {
+                returndata.Add((i, j));
+            }
+        }
+    }
+
+    return returndata;
+}
+
 Console.WriteLine(locationOfGuard);
 
 (int,int) findGuard(string testset) {
