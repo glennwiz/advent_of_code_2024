729dc7b0158e2bad5d2efe9799217d8b193f3303
Glenn Jacobsen <glennwiz@gmail.com>
2024-12-06 22:14:51 +0100

    the plan ROTATE | up -1y | right +1x | down +1y | left -x,,, might be wrong but still the plan

--- day6/Program.cs
+++ day6/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+var dr = new Tools.DataReader();
+var resultText = await dr.read_data("https://adventofcode.com/2024/day/6/input");
+
+
+Console.WriteLine("testing22");
+Console.WriteLine(resultText);
+
+var testset = """....#..............#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+""";
+
+
+var locationOfGuard = findGuard(testset);
+
+int findGuard(string testset) { }
